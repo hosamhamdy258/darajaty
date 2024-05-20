c5a38d721266555d1d8deb0aaa4e83cfd65375db
Hossam Hamdy <hosamhamdy258@gmail.com>
2024-05-20 06:31:16 +0300

    react build path integrated with django

--- frontend/project-js/vite.config.js
+++ frontend/project-js/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,8 @@ export default defineConfig({
     // port: 3000,
   },
   build: {
+    // eslint-disable-next-line no-undef
+    outDir: path.resolve(__dirname, 'templates/dist'), // Ensure output directory matches Docker volume
     rollupOptions: {
       output: {
         entryFileNames: "assets/index.js",
